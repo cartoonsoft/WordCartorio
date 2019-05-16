fd25bb84e7c85236627072d33eff85dd899f63b8
Pedro Henrique Pires <pedrohenriquepires2@hotmail.com>
2019-05-16 11:05:23 -0300

    word

---
+++ AdmCartorioWord/FaixaOpcoes.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace AdmCartorioWord
+{
+    partial class FaixaOpcoes : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public FaixaOpcoes()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.GerenciarCamposGroup = this.Factory.CreateRibbonGroup();
+            this.CbNatureza = this.Factory.CreateRibbonComboBox();
+            this.CbCampo = this.Factory.CreateRibbonComboBox();
+            this.BtnAdicionar = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.GerenciarCamposGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.GerenciarCamposGroup);
+            this.tab1.Label = "Adm Cartório";
+            this.tab1.Name = "tab1";
+            // 
+            // GerenciarCamposGroup
+            // 
+            this.GerenciarCamposGroup.Items.Add(this.CbNatureza);
+            this.GerenciarCamposGroup.Items.Add(this.CbCampo);
+            this.GerenciarCamposGroup.Items.Add(this.BtnAdicionar);
+            this.GerenciarCamposGroup.Label = "Gerenciador de campos";
+            this.GerenciarCamposGroup.Name = "GerenciarCamposGroup";
+            // 
+            // CbNatureza
+            // 
+            this.CbNatureza.Label = "Natureza";
+            this.CbNatureza.MaxLength = 100;
+            this.CbNatureza.Name = "CbNatureza";
+            this.CbNatureza.Text = "Selecione";
+            this.CbNatureza.TextChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.CbNatureza_TextChanged);
+            // 
+            // CbCampo
+            // 
+            this.CbCampo.Label = "Campo";
+            this.CbCampo.Name = "CbCampo";
+            this.CbCampo.TextChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.CbCampo_TextChanged);
+            // 
+            // BtnAdicionar
+            // 
+            this.BtnAdicionar.Label = "Adicionar";
+            this.BtnAdicionar.Name = "BtnAdicionar";
+            this.BtnAdicionar.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.BtnAdicionar_Click);
+            // 
+            // FaixaOpcoes
+            // 
+            this.Name = "FaixaOpcoes";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.FaixaOpcoes_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.GerenciarCamposGroup.ResumeLayout(false);
+            this.GerenciarCamposGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup GerenciarCamposGroup;
+        internal Microsoft.Office.Tools.Ribbon.RibbonComboBox CbNatureza;
+        internal Microsoft.Office.Tools.Ribbon.RibbonComboBox CbCampo;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton BtnAdicionar;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal FaixaOpcoes FaixaOpcoes
+        {
+            get { return this.GetRibbon<FaixaOpcoes>(); }
+        }
+    }
+}
---
+++ AdmCartorioWord/FaixaOpcoes.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Word;
+using Microsoft.Office.Tools.Ribbon;
+using Oracle.ManagedDataAccess.Client;
+
+namespace AdmCartorioWord
+{
+    public partial class FaixaOpcoes : RibbonBase
+    {
+        Dictionary<string, int> naturezaCampo = new Dictionary<string, int>();
+        int IdNatureza;
+        string NomeCampo;
+
+        // Função que inicia o Ribbon
+        private void FaixaOpcoes_Load(object sender, RibbonUIEventArgs e)
+        {
+            RibbonDropDownItem selecione
+                        = Globals
+                        .Factory
+                        .GetRibbonFactory()
+                        .CreateRibbonDropDownItem();
+
+            selecione.Label = "Selecione";
+
+            naturezaCampo.Add("Selecione", 0);
+
+            CbNatureza.Items.Add(selecione);
+            BtnAdicionar.Enabled = false;
+
+            IniciarComboBoxNatureza();
+
+        }
+
+        private void CbNatureza_TextChanged(object sender, RibbonControlEventArgs e)
+        {
+            RibbonComboBox drop = sender as RibbonComboBox;
+
+            IdNatureza = naturezaCampo[drop.Text];
+
+            RecarregarCamposPorNatureza(IdNatureza);
+
+            DesbloquearBloquearBotao();
+
+        }
+
+        private void CbCampo_TextChanged(object sender, RibbonControlEventArgs e)
+        {
+            RibbonComboBox drop = sender as RibbonComboBox;
+            NomeCampo = drop.Text;
+
+            DesbloquearBloquearBotao();
+        }
+
+        private void BtnAdicionar_Click(object sender, RibbonControlEventArgs e)
+        {
+            //Pegando posicao do cursor do teclado
+            int posicao = GetPosicaoCursorDoTeclado();
+
+            //Insere o campo no Word
+            InserirCampoNoWord(posicao, NomeCampo);
+
+            FormFields itens = Globals.ThisAddIn.Application.ActiveDocument.FormFields;
+            foreach (var item in itens)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        #region |Funções Auxiliares com Banco |
+
+        /// <summary>
+        /// Função que recarrega os campos do combobox a partir do id do tipo do ato
+        /// </summary>
+        /// <param name="IdTipoAto">Identificador do tipo do ato (inicial,registro etc.)</param>
+        private void RecarregarCamposPorNatureza(int IdTipoAto)
+        {
+            using (var con = new OracleConnection("Data Source=(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.1.16)(PORT =1521))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME =car12)));Password=senha16ri;User ID=dezesseis_new"))
+            {
+                OracleCommand command
+                    = new OracleCommand("SELECT NOME_CAMPO FROM TB_CAMPOS_TP_ATO WHERE ID_TP_ATO = :ID_TP_ATO", con);
+                con.Open();
+
+                command.Parameters.Add(new OracleParameter("ID_TP_ATO", IdTipoAto));
+
+                CbCampo.Items.Clear();
+                OracleDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    RibbonDropDownItem item
+                        = Globals
+                        .Factory
+                        .GetRibbonFactory()
+                        .CreateRibbonDropDownItem();
+
+                    item.Label = reader.GetValue(0).ToString();
+                    CbCampo.Items.Add(item);
+
+                }
+            }
+        }
+
+        #endregion
+
+        #region | Funções Auxiliares |
+
+        /// <summary>
+        /// Pega a posição absoluta do cursor do teclado
+        /// </summary>
+        /// <returns>posicao do teclado (int) </returns>
+        private static int GetPosicaoCursorDoTeclado()
+        {
+            return Globals.ThisAddIn
+                .Application.Selection.Start;
+        }
+
+        /// <summary>
+        /// Insere um campo no word de acordo com o NomeCampo
+        /// </summary>
+        /// <param name="posicao">posicao que inicia o campo</param>
+        /// <param name="nomeCampo">Nome que fica no placeholder</param>
+        private static void InserirCampoNoWord(int posicao, string nomeCampo)
+        {
+
+            Range posicaoRange;
+
+            //Determina a posicao inicial que o componente sera criado
+            //Trata o erro inesperado colocando o campo na posição do cursor e usando o espaço dele
+            try
+            {
+                posicaoRange
+                    = Globals.ThisAddIn
+                    .Application
+                    .ActiveDocument.Range(posicao, posicao);
+            }
+            catch
+            {
+                posicaoRange
+                    = Globals.ThisAddIn
+                    .Application
+                    .ActiveDocument.Range(posicao);
+            }
+
+
+            //Pega o documento ativo
+            Document documentoAtivo
+                = Globals.ThisAddIn.Application.ActiveDocument;
+            if (documentoAtivo == null)
+                return;
+
+            //Cria as variaveis de configuracao do campo
+            //NomeCampo é uma variavel global que se atualiza com o CbCampo
+            object TipoDoCampo = WdTextFormFieldType.wdRegularText;
+            object TextoCampo = $"nome = {nomeCampo}";
+            object Preservar = false;
+
+            //Adiciona o campo na lista de campo e na posicao determinada
+            FormField input = documentoAtivo.FormFields.Add(posicaoRange,
+                WdFieldType.wdFieldFormTextInput);
+
+            // Campo inicialmente vazio, então ele é considerado erro.
+            input.Name = nomeCampo;
+            input.TextInput.Default = nomeCampo;
+            input.HelpText = nomeCampo;
+            input.Result = $"[{nomeCampo}]";
+
+        }
+
+        private void DesbloquearBloquearBotao()
+        {
+            if (IdNatureza != 0 && NomeCampo != null)
+            {
+                BtnAdicionar.Enabled = true;
+            }
+            else
+            {
+                BtnAdicionar.Enabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Função que inicia o combobox Natureza pegando os dados do banco disponiveis
+        /// </summary>
+        private void IniciarComboBoxNatureza()
+        {
+            using (var con = new OracleConnection("Data Source=(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.1.16)(PORT =1521))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME =car12)));Password=senha16ri;User ID=dezesseis_new"))
+            {
+                OracleCommand command
+                    = new OracleCommand("SELECT ID_TP_ATO,DESCRICAO FROM TB_TP_ATO", con);
+                con.Open();
+                OracleDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    RibbonDropDownItem item
+                        = Globals
+                        .Factory
+                        .GetRibbonFactory()
+                        .CreateRibbonDropDownItem();
+
+                    item.Label = reader.GetValue(1).ToString();
+
+                    naturezaCampo.Add(
+                        reader.GetValue(1).ToString(),
+                        int.Parse(reader.GetValue(0).ToString())
+                        );
+
+                    CbNatureza.Items.Add(item);
+
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
